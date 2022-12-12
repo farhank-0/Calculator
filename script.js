9cfad04ed95eac846499786406bff41d8c44c6fc
farhank-0 <farhankassam0@gmail.com>
2022-12-12 16:57:42 -0500

    Added styles, calculator class, linked functionality
    Used CSS grid to display calculator buttons. Used flexbox to display numbers
    on dark portion of calculator. Created calculator class with methods to
    display numbers and linked with number and operation buttons with event
    listeners. Missing: operate functionality, delete singular number, update
    display with operand.
    
    Commit: [2]

--- script.js
+++ script.js
@@ -1,4 +1,67 @@
-function add(a,b) {
+class Calculator {
+    constructor(prevOpText, currOpText) {
+        this.prevOpText = prevOpText;
+        this.currOpText = currOpText;
+        this.clear();
+    }
+    clear() {
+        this.prevOp = '';
+        this.currOp = '';
+        this.operation = undefined;
+    }
+    delete() {
+
+    }
+    appendNum(num) {
+        if(num === "." && this.currOp.includes('.')){return}
+        this.currOp = this.currOp.toString() + num.toString();
+    }
+    chooseOp(operation) {
+        if(this.currOp === '') {return}
+        if(this.prevOp !== '') {
+            this.operate();
+        }
+        this.operation = operation;
+        this.prevOp = this.currOp;
+        this.currOp = '';
+    }
+    operate() {
+
+    }
+    updateDisplay() {
+        this.currOpText.innerText = this.currOp;
+        this.prevOpText.innerText = this.prevOp;
+    }
+}
+const numButtons = document.querySelectorAll('[data-num]');
+const opButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const delButton = document.querySelector('[data-del]');
+const allClearButton = document.querySelector('[data-allclear]');
+const prevOpText = document.querySelector('[data-previousop]');
+const currOpText = document.querySelector('[data-currentop]');
+
+const calculator = new Calculator(prevOpText, currOpText);
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNum(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+opButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOp(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.operate();
+    calculator.updateDisplay();
+})
+/*function add(a,b) {
     return a+b;
 }
 
@@ -27,4 +90,4 @@ function operate(num1, operand, num2) {
     if(operand == '/') {
         divide(num1, num2);
     }
-}
+}*/
